36e312f88830d1ee0add4a59ce0c50457e30b573
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-02-19 10:01:24 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+"""
+blinking light simulation.
+Light blinks every 1 second.
+Blink time increments by 1 each loop.
+
+created on Feb 2025 by Liya G
+"""
+
+import time 
+import board
+import digitalio
