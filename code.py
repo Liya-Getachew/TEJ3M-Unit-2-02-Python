4c67ea14d451127e88ec895d98babedce3c96a9e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-02-19 13:03:31 -0500

    code

--- code.py
+++ code.py
@@ -1,11 +1,24 @@
 """
-blinking light simulation.
-Light blinks every 1 second.
-Blink time increments by 1 each loop.
+Created by: Liya Getachew
+Created on: February 19 2025
+This makes the LED blink every second.
+The blink time increments by 1 every loop.
 
-created on Feb 2025 by Liya G
 """
-
-import time 
+import time
 import board
 import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+blink_time = 3000
+
+# loop forever
+while True:
+    led.value = True
+    time.sleep(blink_time)
+    led.value = False
+    time.sleep(blink_time)
+
+    blink_time = blink_time + 1000
